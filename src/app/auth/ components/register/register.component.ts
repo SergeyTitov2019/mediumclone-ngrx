a3ac20b24e261c4a58d6d2fc75120bfaa813a0c7
Sergey Titov <sergeyntitov@gmail.com>
2022-08-07 16:04:01 +0300

    add auth [ 002 ]

---
+++ src/app/auth/ components/register/register.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css'],
+})
+export class RegisterComponent {
+  form: any;
+  onSubmit() {}
+}
+
+// import { Component, OnInit } from '@angular/core';
+// import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+//
+// @Component({
+//   selector: 'mc-register',
+//   templateUrl: './register.component.html',
+//   styleUrls: ['./register.component.scss'],
+// })
+// export class RegisterComponent implements OnInit {
+//   form: FormGroup;
+//
+//   constructor(private fb: FormBuilder) {}
+//
+//   ngOnInit(): void {
+//     this.initializeForm();
+//   }
+//
+//   initializeForm(): void {
+//     console.log('initializeForm');
+//     this.form = this.fb.group({
+//       username: ['', Validators.required],
+//       email: ['', Validators.required],
+//       password: ['', Validators.required],
+//     });
+//   }
+//
+//   onSubmit(): void {
+//     console.log('submit', this.form.value, this.form.valid);
+//   }
+// }
