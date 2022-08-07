0580035fdd8ac04b4e3b62acb7ba519d7f28c7ef
Sergey Titov <sergeyntitov@gmail.com>
2022-08-07 16:15:39 +0300

    add auth form [ 003 ]

--- src/app/auth/ components/register/register.component.ts
+++ src/app/auth/ components/register/register.component.ts
@@ -1,42 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css'],
 })
-export class RegisterComponent {
-  form: any;
-  onSubmit() {}
-}
+export class RegisterComponent implements OnInit {
+  form: FormGroup;
 
-// import { Component, OnInit } from '@angular/core';
-// import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-//
-// @Component({
-//   selector: 'mc-register',
-//   templateUrl: './register.component.html',
-//   styleUrls: ['./register.component.scss'],
-// })
-// export class RegisterComponent implements OnInit {
-//   form: FormGroup;
-//
-//   constructor(private fb: FormBuilder) {}
-//
-//   ngOnInit(): void {
-//     this.initializeForm();
-//   }
-//
-//   initializeForm(): void {
-//     console.log('initializeForm');
-//     this.form = this.fb.group({
-//       username: ['', Validators.required],
-//       email: ['', Validators.required],
-//       password: ['', Validators.required],
-//     });
-//   }
-//
-//   onSubmit(): void {
-//     console.log('submit', this.form.value, this.form.valid);
-//   }
-// }
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+  initializeForm(): void {
+    console.log('initializeForm');
+    this.form = this.fb.group({
+      username: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    console.log('submit', this.form.value, this.form.valid);
+  }
+}
